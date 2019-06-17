8a475add166d659b34dac4a16a8d421a8bd78190
martyv4 <51241525+martyv4@users.noreply.github.com>
2019-06-17 13:17:04 -0400

    add variables

--- js/script.js
+++ js/script.js
@@ -2,48 +2,31 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
+
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+/*
+ Two Global constant variables
+  1) studentList:
+     Get the studemt-list from the html page (DOM element). With this
+     querySelector I am looking for any tag with the class name of student-list
+     (querySelector checks for class names when input parameter begins with a period).
+     Then I select only those children (every student list item in the unordered list).
+  2) pageItemCount:
+     I set the number of items to appear in a given page.
+     10 items will appear on each page.
+*/
+const studentList = document.querySelector('.student-list').children;
+const pageItemCount = 10;
 
 
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
+/***
+   Create the `appendPageLinks function` to generate, append, and add
    functionality to the pagination buttons.
 ***/
 
-
+//const showPage = (list, page) =>
 
 
 
